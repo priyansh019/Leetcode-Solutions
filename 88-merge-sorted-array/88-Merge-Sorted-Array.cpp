b62c499db718e081b07c1d026b83ec35338fb008
Priyansh Singh <priyansh.singh019@gmail.com>
2026-08-17 21:33:14 +0530

    [88] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 88-merge-sorted-array/88-Merge-Sorted-Array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res(m + n);
+
+        int i = 0;  // nums1
+        int j = 0;  // nums2
+        int k = 0;  // res
+
+        while(i < m && j < n) {
+            if(nums1[i] < nums2[j]) {
+                res[k] = nums1[i];
+                i++;
+            }
+            else {
+                res[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < m) {
+            res[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while(j < n) {
+            res[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+        nums1 = res;
+    }
+};
