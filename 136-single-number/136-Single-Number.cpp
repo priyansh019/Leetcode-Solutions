67874e3ff1934c8221ed2c54ac70eee7f0486a76
Priyansh Singh <priyansh.singh019@gmail.com>
2026-08-20 23:07:51 +0530

    [136] [Time Beats: 100.00%] [Memory Beats: 24.23%] - LeetPush

--- 136-single-number/136-Single-Number.cpp
+++ 136-single-number/136-Single-Number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int fq[30001]={0};
-        for(int i=0; i<n; i++){
-            fq[nums[i]+10000]++;
-        }   
-        for(int i=0; i<n; i++){
-            if(fq[nums[i]+10000] == 1){
-                return nums[i];
-            }
-        }
-        return -1;
+        int ans = 0;
+
+        for (int x : nums)
+              ans ^= x;
+
+        return ans;
     }
 };
