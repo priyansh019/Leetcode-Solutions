d8f3fb23670f9c13cad2f648f69693d10294dc2a
Priyansh Singh <priyansh.singh019@gmail.com>
2026-08-24 21:48:21 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 75.88%] - LeetPush

--- 169-majority-element/169-Majority-Element.cpp
+++ 169-majority-element/169-Majority-Element.cpp
@@ -3,18 +3,18 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         int co = 0;
-        int can = 0;
+        int candidate = 0;
         for(int i=0; i<n; i++){
             if(co == 0){
-                can = nums[i];
+                candidate = nums[i];
             }
-            if(nums[i]==can){
+            if(nums[i]==candidate){
                 co++;
             }
             else{
                 co--;
             }
         }
-        return can;
+        return candidate;
     }
 };
