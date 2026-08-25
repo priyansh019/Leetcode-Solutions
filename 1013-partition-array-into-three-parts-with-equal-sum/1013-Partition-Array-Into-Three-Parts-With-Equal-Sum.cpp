e733f94ac06344600d310d38e61bd8455ee5fd10
Priyansh Singh <priyansh.singh019@gmail.com>
2026-08-25 15:00:38 +0530

    [1013] [Time Beats: 100.00%] [Memory Beats: 96.63%] - LeetPush

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-Partition-Array-Into-Three-Parts-With-Equal-Sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        int n=arr.size(), sum=0, total=0, co=0, trgt=0;
+        for(int i=0; i<n; i++){
+            total+=arr[i];
+        }
+        if(total%3!=0){
+            return false;
+        }
+        trgt = total/3;
+        for(int i=0; i<n-1; i++){
+            sum += arr[i];
+            if(sum==trgt){
+                co++;
+                sum=0;
+                if(co==2){
+                    return true;
+                }
+            }
+        }
+        // if(co==3){
+        //     return true;
+        // }
+        // else return false;
+        return false;
+    }
+};
