e050decb3fa1ebfb931cd0092c2fc20aa35bd05a
Priyansh Singh <priyansh.singh019@gmail.com>
2026-08-29 22:54:25 +0530

    [674] [Time Beats: 100.00%] [Memory Beats: 29.29%] - LeetPush

---
+++ 674-longest-continuous-increasing-subsequence/674-Longest-Continuous-Increasing-Subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 0) {
+            return 0;
+        }
+
+        int count = 1;
+        int maxcount = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i - 1]) {
+                count++;
+
+                if(count > maxcount) {
+                    maxcount = count;
+                }
+            }
+            else {
+                count = 1;
+            }
+        }
+
+        return maxcount;
+    }
+};
