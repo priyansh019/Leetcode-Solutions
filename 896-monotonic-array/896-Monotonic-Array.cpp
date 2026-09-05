12e0a32724c6e4b3b9701625dfcd7494e73c5630
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-05 21:18:33 +0530

    [896] [Time Beats: 100.00%] [Memory Beats: 33.27%] - LeetPush

--- 896-monotonic-array/896-Monotonic-Array.cpp
+++ 896-monotonic-array/896-Monotonic-Array.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
         int n = nums.size();
-
         bool increasing = true;
         bool decreasing = true;
-
         for(int i = 0; i < n - 1; i++) {
             if(nums[i] > nums[i+1]) {
                 increasing = false;
             }
         }
-
         for(int i = 0; i < n - 1; i++) {
             if(nums[i] < nums[i+1]) {
                 decreasing = false;
             }
         }
-
         if(increasing == true || decreasing == true) {
             return true;
         }
-
         return false;
     }
 };
