fc7efd21e35c73177b37b10be4f878f9ffbdeacf
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-07 21:33:03 +0530

    [1491] [Time Beats: 100.00%] [Memory Beats: 11.14%] - LeetPush

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-Average-Salary-Excluding-the-Minimum-and-Maximum-Salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-Average-Salary-Excluding-the-Minimum-and-Maximum-Salary.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     double average(vector<int>& salary) {
         int n = salary.size();
-
         int min = salary[0];
         int max = salary[0];
 
@@ -11,7 +10,6 @@ public:
             if(salary[i] < min) {
                 min = salary[i];
             }
-
             if(salary[i] > max) {
                 max = salary[i];
             }
@@ -20,14 +18,12 @@ public:
         // Find sum excluding min and max
         int sum = 0;
         int count = 0;
-
         for(int i = 0; i < n; i++) {
             if(salary[i] != min && salary[i] != max) {
                 sum += salary[i];
                 count++;
             }
         }
-
         return (double)sum / count;
     }
 };
