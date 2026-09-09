3c59bbdf4b339bfbdc469930f67e86617e32cbe1
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-09 23:19:04 +0530

    [3978] [Time Beats: 100.00%] [Memory Beats: 64.82%] - LeetPush

--- 3978-unique-middle-element/3978-Unique-Middle-Element.cpp
+++ 3978-unique-middle-element/3978-Unique-Middle-Element.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     bool isMiddleElementUnique(vector<int>& nums) {
         int n = nums.size();
-
         int middle = nums[n / 2];
         int count = 0;
-
         for(int i = 0; i < n; i++) {
             if(nums[i] == middle) {
                 count++;
             }
         }
-
         if(count == 1) {
             return true;
         }
-
         return false;
     }
 };
