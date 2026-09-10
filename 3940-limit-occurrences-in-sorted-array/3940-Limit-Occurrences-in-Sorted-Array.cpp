b3c47512bf211bc8c3cae5470b1b1abc000c6dfe
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-10 22:32:51 +0530

    [3940] [Time Beats: 100.00%] [Memory Beats: 54.94%] - LeetPush

--- 3940-limit-occurrences-in-sorted-array/3940-Limit-Occurrences-in-Sorted-Array.cpp
+++ 3940-limit-occurrences-in-sorted-array/3940-Limit-Occurrences-in-Sorted-Array.cpp
@@ -6,19 +6,16 @@ public:
         int co = 0;
 
         for(int i = 0; i < n; i++) {
-
             if(i == 0 || nums[i] != nums[i-1]) {
                 co = 1;
             }
             else {
                 co++;
             }
-
             if(co <= k) {
                 ans.push_back(nums[i]);
             }
         }
-
         return ans;
     }
 };
