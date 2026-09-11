c7089ab75d6f3d7dac50ee17c9a2a976db80e1b9
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-11 12:16:55 +0530

    [3712] [Time Beats: 100.00%] [Memory Beats: 97.55%] - LeetPush

--- 3712-sum-of-elements-with-frequency-divisible-by-k/3712-Sum-of-Elements-With-Frequency-Divisible-by-K.cpp
+++ 3712-sum-of-elements-with-frequency-divisible-by-k/3712-Sum-of-Elements-With-Frequency-Divisible-by-K.cpp
@@ -3,21 +3,17 @@ public:
     int sumDivisibleByK(vector<int>& nums, int k) {
         int n = nums.size();
         int co = 0, sum = 0;
-
         for(int i = 0; i < n; i++) {
             co = 0;
-
             for(int j = 0; j < n; j++) {
                 if(nums[i] == nums[j]) {
                     co++;
                 }
             }
-
             if(co % k == 0) {
                 sum += nums[i];
             }
         }
-
         return sum;
     }
 };
