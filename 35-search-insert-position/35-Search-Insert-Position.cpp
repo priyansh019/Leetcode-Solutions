b2c4fb69980a93a5c8ad014e2de0759c73f146e1
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-12 17:53:07 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 77.97%] - LeetPush

---
+++ 35-search-insert-position/35-Search-Insert-Position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        while(s<=e){
+            int m = (s+e)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                s = m+1;
+            }
+            else
+                e = m-1;
+        }
+        return s;
+    }
+};
