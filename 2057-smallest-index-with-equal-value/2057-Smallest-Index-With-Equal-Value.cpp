e1c20ff1fab43af3dcdc7b1d46f0bfb6b50f09f8
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-14 21:58:14 +0530

    [2057] [Time Beats: 100.00%] [Memory Beats: 43.81%] - LeetPush

--- 2057-smallest-index-with-equal-value/2057-Smallest-Index-With-Equal-Value.cpp
+++ 2057-smallest-index-with-equal-value/2057-Smallest-Index-With-Equal-Value.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
         int n = nums.size();
-
         for(int i = 0; i < n; i++) {
             if(i % 10 == nums[i]) {
                 return i;
             }
         }
-
         return -1;
     }
 };
