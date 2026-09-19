4605a0c2540db88734d46cc796bd689efc20add2
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-19 21:49:43 +0530

    [1588] [Time Beats: 18.73%] [Memory Beats: 21.41%] - LeetPush

--- 1588-sum-of-all-odd-length-subarrays/1588-Sum-of-All-Odd-Length-Subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-Sum-of-All-Odd-Length-Subarrays.cpp
@@ -3,12 +3,9 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int n = arr.size();
         int sum = 0;
-
         for(int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
-
                 int length = j - i + 1;
-
                 if(length % 2 == 1) {
                     for(int k = i; k <= j; k++) {
                         sum += arr[k];
@@ -16,7 +13,6 @@ public:
                 }
             }
         }
-
         return sum;
     }
 };
