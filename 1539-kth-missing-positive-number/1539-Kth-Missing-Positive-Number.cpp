635afebf52ba833e54677cf0b5eed4222a211b3f
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-20 23:25:47 +0530

    [1539] [Time Beats: 100.00%] [Memory Beats: 29.35%] - LeetPush

--- 1539-kth-missing-positive-number/1539-Kth-Missing-Positive-Number.cpp
+++ 1539-kth-missing-positive-number/1539-Kth-Missing-Positive-Number.cpp
@@ -1,31 +1,57 @@
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int n = arr.size();
+//         int x = 1;
+
+//         while(k > 0) {
+
+//             bool found = false;
+
+//             for(int i = 0; i < n; i++) {
+//                 if(arr[i] == x) {
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if(found == false) {
+//                 k--;
+//             }
+
+//             if(k == 0) {
+//                 return x;
+//             }
+
+//             x++;
+//         }
+
+//         return x;
+//     }
+// };
+
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
         int x = 1;
 
-        while(k > 0) {
+        for(int i = 0; i < n; i++) {
 
-            bool found = false;
-
-            for(int i = 0; i < n; i++) {
-                if(arr[i] == x) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if(found == false) {
+            while(x < arr[i]) {
                 k--;
-            }
 
-            if(k == 0) {
-                return x;
+                if(k == 0) {
+                    return x;
+                }
+
+                x++;
             }
 
             x++;
         }
 
-        return x;
+        // Missing numbers after the end of arr
+        return x + k - 1;
     }
 };
