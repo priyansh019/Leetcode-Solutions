ec164bb87570ec6d71cff9260da3d1e88fb1f6e4
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-21 22:21:23 +0530

    [1431] [Time Beats: 100.00%] [Memory Beats: 60.07%] - LeetPush

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        int maxi = candies[0];
+
+        // Find maximum candies
+        for(int i = 0; i < n; i++) {
+            if(candies[i] > maxi) {
+                maxi = candies[i];
+            }
+        }
+
+        vector<bool> ans;
+
+        // Check each kid
+        for(int i = 0; i < n; i++) {
+            if(candies[i] + extraCandies >= maxi) {
+                ans.push_back(true);
+            }
+            else {
+                ans.push_back(false);
+            }
+        }
+
+        return ans;
+    }
+};
