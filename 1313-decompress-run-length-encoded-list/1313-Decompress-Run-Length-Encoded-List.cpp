51b0f1399be821374e97f8b86c660b37b96c30f3
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-22 23:35:28 +0530

    [1313] [Time Beats: 100.00%] [Memory Beats: 71.91%] - LeetPush

--- 1313-decompress-run-length-encoded-list/1313-Decompress-Run-Length-Encoded-List.cpp
+++ 1313-decompress-run-length-encoded-list/1313-Decompress-Run-Length-Encoded-List.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> decompressRLElist(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-
         for(int i = 0; i < n; i += 2) {
             int freq = nums[i];
             int val = nums[i + 1];
-
             for(int j = 0; j < freq; j++) {
                 ans.push_back(val);
             }
