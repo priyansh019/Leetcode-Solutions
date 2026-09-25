cfb7842921c0963097132cf89e0a6d18f7b4e673
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-25 20:20:14 +0530

    [1460] [Time Beats: 43.04%] [Memory Beats: 93.35%] - LeetPush

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-Make-Two-Arrays-Equal-by-Reversing-Subarrays.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         int n = target.size();
-
         for(int i = 0; i < n; i++) {
             bool found = false;
-
             for(int j = 0; j < n; j++) {
                 if(target[i] == arr[j]) {
                     arr[j] = -1;
@@ -13,12 +11,10 @@ public:
                     break;
                 }
             }
-
             if(found == false) {
                 return false;
             }
         }
-
         return true;
     }
 };
