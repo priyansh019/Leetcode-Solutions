8d3a64e1e1f5c644faa3a4bb567fd17ca21289d8
Priyansh Singh <priyansh.singh019@gmail.com>
2026-09-26 20:59:18 +0530

    [1450] [Time Beats: 100.00%] [Memory Beats: 85.90%] - LeetPush

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-Number-of-Students-Doing-Homework-at-a-Given-Time.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n=endTime.size();
+        int co=0;
+        for(int i=0; i<n; i++){
+            if(startTime[i]<=queryTime && endTime[i]>=queryTime){
+                co++;
+            }
+        }
+        return co;
+    }
+};
